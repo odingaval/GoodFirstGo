5c499a7bfd22d9b54f94b0e5fb8b8a433a67d388
odingaval <odingaval71@gmail.com>
2026-03-04 08:56:29 +0300

    First implemetation of main.go

--- cmd/goodfirstgo/main.go
+++ cmd/goodfirstgo/main.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+	limit := flag.Int("limit", 5, "Number of issue to fetch") // this will give a refrence to the int which will later hold the parsed value
+
+	flag.Parse() //updated the parsed value at *int
+	fmt.Printf("Fetchng %d good first issues... \n", *limit)
+}
