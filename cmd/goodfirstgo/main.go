3fc6f1f7bf429d74326100175ab181222551482f
odingaval <odingaval71@gmail.com>
2026-03-11 08:02:01 +0300

    feat:implement  Github API client for fetching good first issues

--- cmd/goodfirstgo/main.go
+++ cmd/goodfirstgo/main.go
@@ -3,11 +3,19 @@ package main
 import (
 	"flag"
 	"fmt"
+
+	"GoodFirstGo/internal/github"
 )
 
 func main() {
 	limit := flag.Int("limit", 5, "Number of issue to fetch") // this will give a refrence to the int which will later hold the parsed value
+	flag.Parse()                                              //updated the parsed value at *int
 
-	flag.Parse() //updated the parsed value at *int
 	fmt.Printf("Fetchng %d good first issues... \n", *limit)
+	client := github.NewClient()
+
+	err := client.SearchGoodFirstIssues()
+	if err != nil {
+		fmt.Println("Error fetching issues:", err)
+	}
 }
--- internal/github/client.go
+++ internal/github/client.go
@@ -2,6 +2,7 @@ package github
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 )
 
@@ -9,24 +10,30 @@ type Client struct { //deffining a custom type client (github API client)
 	httpClient *http.Client
 }
 
-func NewClient() *Client { //creating a new client, wrapping it inside Client
+func NewClient() *Client { //creating a new client instance, wrapping it inside Client
 	return &Client{ //returning a pointer to it
 		httpClient: &http.Client{},
 	}
 }
 
 func (c *Client) SearchGoodFirstIssues() error { // method on the client
-	url := `https://api.github.com/search/issues?q=label:"good first issue"+language:go`
+	url := `https://api.github.com/search/issues?q=label:"good-first-issue"+language:go&per_page=5`
 
-	resp, err := c.httpClient.Get(url)
+	resp, err := c.httpClient.Get(url) //making a get request using embedded http.Client
 	if err != nil {
-		return fmt.Errorf("faile to make a request: %w", err)
+		return fmt.Errorf("feild to make a request: %w", err)
 	}
-	defer resp.Body.Close()
+	defer resp.Body.Close() //closing the response body after the function returns to prevent resource leaks
 
 	fmt.Println("Github API response status:", resp.Status)
 
-	return nil
+	body, err := io.ReadAll(resp.Body) //Reading the response body
+	if err != nil {
+		return err
+	}
 
+	fmt.Println(string(body))
+
+	return nil
 
 }
