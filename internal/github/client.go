139d17efbfa4ed23a328011e67dd8186eea2941d
odvalery <odingal71@gmail.com>
2026-03-11 15:09:21 +0300

    Update main.go to recieve the the issues

--- internal/github/client.go
+++ internal/github/client.go
@@ -1,32 +1,43 @@
 package github
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
+
+	"GoodFirstGo/internal/models"
 )
 
-type Client struct { //deffining a custom type client (github API client)
+type Client struct { // deffining a custom type client (github API client)
 	httpClient *http.Client
 }
 
-func NewClient() *Client { //creating a new client, wrapping it inside Client
-	return &Client{ //returning a pointer to it
-		httpClient: &http.Client{},
+func NewClient() *Client { // creating a new client, wrapping it inside Client
+	return &Client{ // returning a pointer to it
+		httpClient: &http.Client{
+			Timeout: 10 * time.Second,
+		},
 	}
 }
 
-func (c *Client) SearchGoodFirstIssues() error { // method on the client
-	url := `https://api.github.com/search/issues?q=label:"good first issue"+language:go`
+func (c *Client) SearchGoodFirstIssues() ([]models.Issue, error) { // method on the client
+	url := `https://api.github.com/search/issues?q=label:"good-first-issue"+language:go:page=5`
+
+	fmt.Println("Making a request to GitHub API...")
 
 	resp, err := c.httpClient.Get(url)
 	if err != nil {
-		return fmt.Errorf("faile to make a request: %w", err)
+		return nil, fmt.Errorf("failed to make a request: %w", err)
 	}
 	defer resp.Body.Close()
 
-	fmt.Println("Github API response status:", resp.Status)
+	var result models.SearchResponses
 
-	return nil
+	err = json.NewDecoder(resp.Body).Decode(&result)
+	if err != nil {
+		return nil, err
+	}
 
-
+	return result.Items, nil
 }
