90e5455671a550840b133a413f0bfd535de43d19
odingaval <odingaval71@gmail.com>
2026-03-28 09:05:01 +0300

    Extract issue printing logic to output package

--- pkg/output/printer.go
+++ pkg/output/printer.go
@@ -1,1 +1,14 @@
-package pkg
+package output
+
+import (
+	"fmt"
+
+	"GoodFirstGo/internal/models"
+)
+
+func PrintIssues(issues []models.Issue) {
+	fmt.Printf("Found %d good first issues:\n", len(issues))
+	for _, issue := range issues {
+		fmt.Printf("- %s (Repo: %s) [%s]\n", issue.Title, issue.Repo.FullName, issue.HTMLURL)
+	}
+}
