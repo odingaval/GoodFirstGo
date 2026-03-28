178aaea6a44448266fa0f522abeead25216730ec
odingaval <odingaval71@gmail.com>
2026-03-28 09:05:25 +0300

    Refactor CLI with cobra framework

--- internal/models/issue.go
+++ internal/models/issue.go
@@ -1,11 +1,11 @@
 package models
 
 type Issue struct {
-	ID       int    `json:"id"`
-	Number   int    `json:"number"`
-	Title    string `json:"title"`
-	HTMLURL  string `json:"html_url"`
-	Repo     struct {
+	ID      int    `json:"id"`
+	Number  int    `json:"number"`
+	Title   string `json:"title"`
+	HTMLURL string `json:"html_url"`
+	Repo    struct {
 		FullName string `json:"full_name"`
 	} `json:"repository"`
 }
